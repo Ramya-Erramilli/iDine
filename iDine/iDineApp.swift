b74d63cce400e4ad3532413381239d8c7166a8da
Ramya Erramilli <e.ramya.103@gmail.com>
2021-09-05 22:30:01 +0530

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Ramya - Personal on 05/09/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
